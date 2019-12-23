9f4b6ef97af5ddcce8eb61e7fc51c4749270741b
DoThanhLoc <loc.do@nextfunc.com>
2019-12-23 23:47:17 +0700

    refactor code

--- Project/DTO/ChiTietBaoCaoSachTraTre.vb
+++ Project/DTO/ChiTietBaoCaoSachTraTre.vb
@@ -18,56 +18,22 @@
     Public Sub New()
     End Sub
 
-     Public Sub New(maChiTietPhieuMuonSach As Integer, maPhieuMuonSach As Integer, maSach As Integer)
-        Me.MaChiTietPhieuMuonSach = maChiTietPhieuMuonSach
-        Me.MaPhieuMuonSach = maPhieuMuonSach
-        Me.MaSach = maSach
-    End Sub
-    Public Sub New()
-    End Sub
-
-    Public Sub New(row As DataRow)
-        Dim doesRowContainsCorrectFields = row.Table.Columns.Contains("MaChiTietPhieuMuonSach") And
-          row.Table.Columns.Contains("MaPhieuMuonSach") And
-        row.Table.Columns.Contains("MaSach")
-        If doesRowContainsCorrectFields = False Then
-            Return
-        End If
-
-        MaPhieuMuonSach = row("MaPhieuMuonSach").ToString()
-        MaChiTietPhieuMuonSach = row("MaChiTietPhieuMuonSach").ToString()
-        MaSach = row("MaSach").ToString()
-    End Sub
-     Public Sub New(row As DataRow)
-        Dim doesRowContainsCorrectFields = row.Table.Columns.Contains("MaChiTietPhieuMuonSach") And
-          row.Table.Columns.Contains("MaPhieuMuonSach") And
-        row.Table.Columns.Contains("MaSach")
-        If doesRowContainsCorrectFields = False Then
-            Return
-        End If
-
-        MaPhieuMuonSach = row("MaPhieuMuonSach").ToString()
-        MaChiTietPhieuMuonSach = row("MaChiTietPhieuMuonSach").ToString()
-        MaSach = row("MaSach").ToString()
-    End Sub
-
-       Public Sub New(maBaoCaoSachTraTre As String, thoiGian As Date)
-        Me.MaBaoCaoSachTraTre = maBaoCaoSachTraTre
-        Me.ThoiGian = thoiGian
-    End Sub
-
-    Public Sub New()
-    End Sub
     Public Sub New(row As DataRow)
         Dim doesRowContainsCorrectFields As Boolean = row.Table.Columns.Contains("MaBaoCaoSachTraTre") And
-         row.Table.Columns.Contains("ThoiGian")
+         row.Table.Columns.Contains("SoNgayTre") And
+          row.Table.Columns.Contains("MaChiTietBaoCaoSachTraTre") And
+          row.Table.Columns.Contains("MaChiTietPhieuMuonSach")
         If doesRowContainsCorrectFields = False Then
             MaBaoCaoSachTraTre = 0
-            ThoiGian = DateTime.Now
+            MaChiTietBaoCaoSachTraTre = 0
+            MaChiTietPhieuMuonSach = 0
+            SoNgayTraTre = 0
             Return
         End If
-        MaBaoCaoSachTraTre = row("MaBaoCaoSachTraTre")
-        DateTime.TryParse(row("ThoiGian").ToString(), ThoiGian)
+        MaBaoCaoSachTraTre = row("MaBaoCaoSachTraTre").ToString()
+        MaBaoCaoSachTraTre = row("MaChiTietBaoCaoSachTraTre").ToString()
+        MaChiTietPhieuMuonSach = row("MaChiTietPhieuMuonSach").ToString()
+        Integer.TryParse(row("SoNgayTre").ToString(), SoNgayTraTre)
     End Sub
 #End Region
 End Class
