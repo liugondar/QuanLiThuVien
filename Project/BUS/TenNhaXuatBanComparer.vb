3199001302c44774f18a760928735e4a23e3b9e3
giaphat <16520910@gmail.edu.uit.vn>
2019-12-24 18:52:02 +0700

    them ten nxb

--- Project/BUS/TenNhaXuatBanComparer.vb
+++ Project/BUS/TenNhaXuatBanComparer.vb
@@ -6,7 +6,9 @@ Public Class TenNhaXuatBanComparer
     Public Function Equals(x As DauSachDTO, y As DauSachDTO) As Boolean Implements IEqualityComparer(Of DauSachDTO).Equals
         Return x.TenNhaXuatBan.Equals(y.TenNhaXuatBan)
     End Function
-
+    Public Function Equals(x As DauSachDTO, y As DauSachDTO) As Boolean Implements IEqualityComparer(Of DauSachDTO).Equals
+        Return x.TenNhaXuatBan.Equals(y.TenNhaXuatBanComparer)
+    End Function
     Public Function GetHashCode(obj As DauSachDTO) As Integer Implements IEqualityComparer(Of DauSachDTO).GetHashCode
         Return obj.TenNhaXuatBan.GetHashCode()
     End Function
