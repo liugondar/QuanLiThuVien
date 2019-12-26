18cc4d94401a51ee91cd44ff0286bb6ddcb75c84
liugondar <liugondar@gmail.com>
2019-12-27 01:44:36 +0700

    complete search

--- Project/BUS/QuanLiSachBus.vb
+++ Project/BUS/QuanLiSachBus.vb
@@ -1,6 +1,7 @@
 ﻿Imports DAO
 Imports DTO
 Imports Utility
+Imports System.Linq
 Public Class QuanLiSachBus
 
     Private Shared oInstance As QuanLiSachBus = New QuanLiSachBus
@@ -14,7 +15,12 @@ Public Class QuanLiSachBus
         End Get
     End Property
 
-    Public Function SearchSachByString(query As String, ByRef dausach As List(Of DauSachDTO)) As Result
-        Return DAO.QuanLiSachDAO.Instance.SearchSachByString(query, dausach)
+    Public Function SearchSachByString(query As String, ByRef listDauSach As List(Of DauSachDTO)) As Result
+        Dim rs = DAO.QuanLiSachDAO.Instance.SearchSachByString(query, listDauSach)
+        Dim comparer = New SachIdAndNameComparer()
+        listDauSach = listDauSach.Distinct(comparer).ToList()
+
+
+        Return rs
     End Function
 End Class
---
+++ Project/DTO/SachIdAndNameComparer.vb
@@ -0,0 +1,12 @@
+﻿Imports DTO
+Public Class SachIdAndNameComparer
+    Implements IEqualityComparer(Of DauSachDTO)
+
+    Public Function Equals(x As DauSachDTO, y As DauSachDTO) As Boolean Implements IEqualityComparer(Of DauSachDTO).Equals
+        Return x.MaDauSach.Equals(y.MaDauSach) And x.TenSach.Equals(y.TenSach)
+    End Function
+
+    Public Function GetHashCode(obj As DauSachDTO) As Integer Implements IEqualityComparer(Of DauSachDTO).GetHashCode
+        Return obj.MaDauSach.GetHashCode()
+    End Function
+End Class
